a13ebca77532449b33deeeb62a8e1717428971f2
Al1bi <denilson.fiesta@ucb.edu.bo>
2023-08-08 13:43:43 -0400

    Funcionalidad de multiplicacion agregada

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,27 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+//const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const sumBtn = document.getElementById("sumar-button");
+const multiplyBtn = document.getElementById("multiply-button")
 
-form.addEventListener("submit", (event) => {
+sumBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  
+  div.innerHTML = "<p>Resultado de la suma: " + sumar(firstNumber, secondNumber) + "</p>";
+});
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+multiplyBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+  
+  div.innerHTML = "<p>Resultado de la multiplicación: " + multiplicar(firstNumber, secondNumber) + "</p>";
 });
